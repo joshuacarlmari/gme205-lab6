0d99142f43d4291d008851dd01af51ffcc6789e7
joshuacarlmari <joshuaarlmarifa@gmail.com>
2026-04-30 18:53:49 +0800

    Parcel Class Generated

---
+++ models/parcel.py
@@ -0,0 +1,29 @@
+from spatial object import SpatialObject
+
+class Parcel(SpatialObject):
+
+    def __init__(self, parcel_id, geometry, area, zone):
+        super().__init__(geometry)
+        self.parcel_id = parcel_id
+        self.area = area
+        self.zone = zone
+        self.buildings = []
+        self.adjacent_roads = []
+
+    def compute_area(self):
+        return self.area
+    
+    def add_building(self, building):
+        if building not in self.buildings:
+            self.buildings.append(building)
+
+    def add_adjacent_road(self, road):
+        if road not in self.adjacent_roads:
+            self.adjacent_roads.append(road)
+
+    def describe(self):
+        return (
+            f"Parcel {self.parcel_id}: zone={self.zone}, "
+            f"area={self.area}, buildings={len(self.buildings)}, "
+            f"adjacent_roads={len(self.adjacent_roads)}"
+        )
