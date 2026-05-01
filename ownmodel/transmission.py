7ce9c137ded45dfb041949672834c6b03e6f8bc5
joshuacarlmari <joshuacarlmarifa@gmail.com>
2026-05-01 23:59:07 +0800

    Update transmission.py

--- ownmodel/transmission.py
+++ ownmodel/transmission.py
@@ -8,10 +8,7 @@ class TransmissionLineEasement(SpatialObject):
         self.voltage_kv = voltage_kv
 
     def get_corridor(self):
-        """
-        Returns the buffered corridor geometry around the transmission line.
-        Width is divided by 2 since buffer expands equally on both sides.
-        """
+        
         return self.geometry.buffer(self.width_m / 2)
 
     def get_id(self):
